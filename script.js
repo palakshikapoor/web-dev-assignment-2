758dd4e01b8b45fac0d76b65273b836837ffa855
Palakshi <palakshikapoor03@gmail.com>
2026-03-15 18:38:01 +0530

    Created JavaScript file and initialized variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const weatherDiv = document.getElementById("weather");
+const historyDiv = document.getElementById("history");
+const consoleBox = document.getElementById("console");
+
+console.log("Weather Tracker Loaded");
